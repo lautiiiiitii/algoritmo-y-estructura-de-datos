4fd802d9ffe091fd68deaf988a01f0232fd2e789
lautiiiiitii <lautaro.desantisset32@gmail.com>
2025-07-11 11:08:32 -0300

    Update tp-matrices_Lautaro_DeSantisPT4.py

--- tp-matrices_Lautaro_DeSantisPT4.py
+++ tp-matrices_Lautaro_DeSantisPT4.py
@@ -1,12 +1,10 @@
-
-# matrices_parte4.py
-
-# Ejercicio 16: Resta de dos matrices de números decimales
+# Ejercicio 16: Escribir un método que dadas dos matrices de dobles devuelva una nueva matriz que represente la resta de 
+#las matrices que se le pasaron.
 def resta_matrices(m1, m2):
     filas, columnas = len(m1), len(m1[0])
     return [[m1[i][j] - m2[i][j] for j in range(columnas)] for i in range(filas)]
 
-# Ejercicio 17: Producto de dos matrices de números decimales
+# Ejercicio 17: Escribir un método que dadas dos matrices de dobles devuelva una nueva matriz que represente el producto de las matrices que se le pasaron.
 def producto_matrices(m1, m2):
     filas_m1, columnas_m1 = len(m1), len(m1[0])
     filas_m2, columnas_m2 = len(m2), len(m2[0])
@@ -19,11 +17,11 @@ def producto_matrices(m1, m2):
                 resultado[i][j] += m1[i][k] * m2[k][j]
     return resultado
 
-# Ejercicio 18: Transpuesta de una matriz de números decimales
+# Ejercicio 18: Escribir un método que dada una matriz de dobles transponga la matriz.
 def transponer_matriz(matriz):
     return [list(fila) for fila in zip(*matriz)]
 
-# Ejercicio 19: Inversa de una matriz cuadrada de números decimales
+# Ejercicio 19: Escribir un método que dada una matriz calcule su inversa.
 def inversa_matriz(matriz):
     import numpy as np
     matriz_np = np.array(matriz)
